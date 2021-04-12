e9ff01556fe4f0746fe85b67a8710d1ec377757e
Diogo Dourado <dcdourado@gmail.com>
2021-04-12 20:37:36 -0300

    style: organize variables and method params

--- src/Node/index.js
+++ src/Node/index.js
@@ -1,16 +1,11 @@
-import React, {
-  useCallback,
-  useContext,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { nanoid } from "nanoid";
 import Styles from "./style.module.css";
 
 const Node = (props) => {
   const { name, fatherId, children } = props;
 
+  const self = useRef();
   const [selfId, _UNSAFE_setSelfId] = useState(nanoid());
   const [hasMounted, setMounted] = useState(false);
 
@@ -20,22 +15,19 @@ const Node = (props) => {
     clearNodeLine: (a, b) => {},
     drawLineBetweenNodes: (a, b) => {},
   };
-  const self = useRef();
 
   // @doc "Waits component screen loading"
   useLayoutEffect(() => {
     if (hasMounted) {
+      // NO-OP
       return;
     }
 
-    Tree.pushNode({
-      self: { ...self, id: selfId },
-      fatherId,
-    });
-    Tree.clearNodeLine(fatherId, selfId);
-    Tree.drawLineBetweenNodes(fatherId, selfId);
+    Tree.pushNode({ self, selfId, fatherId });
+    Tree.clearNodeLine({ selfId, fatherId });
+    Tree.drawLineBetweenNodes({ selfId, fatherId });
     setMounted(true);
-  }, [hasMounted, Tree, fatherId, selfId]);
+  }, [hasMounted, Tree, selfId, fatherId]);
 
   return (
     <div ref={self} className={Styles.self}>
