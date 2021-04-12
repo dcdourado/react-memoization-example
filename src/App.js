745bbd42df80a1f9f2f5b838ea247acd028f03f3
Diogo Dourado <dcdourado@gmail.com>
2021-04-12 19:59:57 -0300

    feat: Node base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Node from './Node';
+import Node from "./Node";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
 
-      <Node name="Child"/>
+      <Node name="Apartamento">
+        <Node name="Humanos">
+          <Node name="Pati" />
+          <Node name="Diogo" />
+        </Node>
+        <Node name="Gatas">
+          <Node name="Shimeji" />
+          <Node name="Tufinha" />
+        </Node>
+        <Node name="Akita" />
+      </Node>
+
+      {/* <Node name="Grandfather 1">
+        <Node name="Father 1">
+          <Node name="Child 1" />
+          <Node name="Child 2" />
+        </Node>
+        <Node name="Father 2">
+          <Node name="Child 3" />
+        </Node>
+      </Node> */}
     </div>
   );
 }
--- src/Node/index.js
+++ src/Node/index.js
@@ -1,11 +1,48 @@
-import Styles from './style.module.css'
+import React, {
+  useCallback,
+  useContext,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { nanoid } from "nanoid";
+import Styles from "./style.module.css";
 
 const Node = (props) => {
-  const { name } = props;
+  const { name, fatherId, children } = props;
+
+  const [selfId, _UNSAFE_setSelfId] = useState(nanoid());
+  const [hasMounted, setMounted] = useState(false);
+
+  // const Tree = useContext();
+  const Tree = {
+    pushNode: () => {},
+    clearNodeLine: (a, b) => {},
+    drawLineBetweenNodes: (a, b) => {},
+  };
+  const self = useRef();
+
+  // @doc "Waits component screen loading"
+  useLayoutEffect(() => {
+    if (hasMounted) {
+      return;
+    }
+
+    Tree.pushNode({
+      self: { ...self, id: selfId },
+      fatherId,
+    });
+    Tree.clearNodeLine(fatherId, selfId);
+    Tree.drawLineBetweenNodes(fatherId, selfId);
+    setMounted(true);
+  }, [hasMounted, Tree, fatherId, selfId]);
 
   return (
-    <div className={Styles.container}>
-      <span>{name}</span>
+    <div ref={self} className={Styles.self}>
+      <div className={Styles.leaf}>
+        <span>{name}</span>
+      </div>
+      <div className={Styles.children}>{children}</div>
     </div>
   );
 };
