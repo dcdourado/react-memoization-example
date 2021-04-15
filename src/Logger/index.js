69482795ac8092d80bcc9d9d6a0b4f40b34fcc85
Diogo Dourado <dcdourado@gmail.com>
2021-04-14 22:53:39 -0300

    chore: apply context and init Tree

--- src/Logger/index.js
+++ src/Logger/index.js
@@ -1,21 +1,21 @@
 const Logger = {
   info: (message) => {
-    console.info(`[${time}] ${message}`);
+    console.info(`[${time()}] ${message}`);
   },
 
   debug: (message) => {
-    console.debug(`[${time}] ${message}`);
+    console.debug(`[${time()}] ${message}`);
   },
 
   warn: (message) => {
-    console.warn(`[${time}] ${message}`);
+    console.warn(`[${time()}] ${message}`);
   },
 
   error: (message) => {
-    console.error(`[${time}] ${message}`);
+    console.error(`[${time()}] ${message}`);
   },
 };
 
-const time = () => new Date().toISOString();
+const time = () => new Date().toLocaleTimeString();
 
 export default Logger;
--- src/Tree/index.js
+++ src/Tree/index.js
@@ -1,25 +1,33 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Logger from "../Logger";
 import Actions from "./actions";
 
 const Context = React.createContext();
 
-export const Tree = (props) => {
+export const TreeContext = (props) => {
   const { children } = props;
 
   const [nodes, setNodes] = useState([]);
   const [edges, setEdges] = useState([]);
 
-  Logger.info(`Node count: ${nodes.length}`)
-  Logger.info(`Edge count: ${edges.length}`)
+  Logger.info(`Node count: ${nodes.length}`);
+  Logger.info(`Edge count: ${edges.length}`);
+
+  useEffect(() => {
+    Logger.info("Tree initialized");
+  }, []);
 
   const pushNode = ({ self, selfId, fatherId }) => {
     const node = Actions.generateNode(self, selfId);
     const plusChildNodes = Actions.appendChild(nodes, node, fatherId);
     setNodes(plusChildNodes);
 
-    const refreshedEdges = Actions.refreshDownEdges(plusChildNodes, [], fatherId);
+    const refreshedEdges = Actions.refreshDownEdges(
+      plusChildNodes,
+      [],
+      fatherId
+    );
     setEdges(refreshedEdges);
 
     Logger.info(`Node ${selfId} pushed successfully`);
@@ -35,5 +43,4 @@ export const Tree = (props) => {
   return <Context.Provider value={value}>{children}</Context.Provider>;
 };
 
-
-export const useTree = () => useContext(Tree);
+export const useTree = () => useContext(TreeContext);
