29c68b5b47f89d14fbfb2c03e6ff9f8c285012ca
Diogo Dourado <dcdourado@gmail.com>
2021-08-18 22:50:02 -0300

    chore: just generateEdge

--- src/Tree/actions.js
+++ src/Tree/actions.js
@@ -8,68 +8,29 @@ const generateNode = (self, id, fatherId) => ({
   fatherId,
 });
 
-const NOTHING = [];
-const refreshDownEdges = (nodes, siblings, selectedNodeId, result = []) => {
-  // Select node
-  let nodeId = selectedNodeId;
-
-  // No where to go...
-  const noWhereToGo = nodeId === undefined && siblings.length === 0;
-  if (noWhereToGo) {
-    Logger.info("[refreshDownEdges] Reached last branch leaf...");
-
-    // With Array.flat() this will go away...
-    return NOTHING;
-  }
-
-  // Node id exists
-  const hasNode = nodeId !== undefined;
-  if (hasNode) {
-    const node = findNodeById(nodes, nodeId);
-
-    const loadedChildren = loadChildren(nodes, node?.children);
-
-    // Builds father => children edges
-    const childrenEdges = loadedChildren
-      .map((child) => generateEdge(node.self, child.self))
-      .flat();
-
-    Logger.info(
-      `[refreshDownEdges] Calculated ${childrenEdges.length} children edges`
-    );
-
-    // Returns it with the rest
-    return childrenEdges;
-  }
-
-  const hasSiblings = siblings.length > 0;
-  if (!hasSiblings) {
-    Logger.warn("[refreshDownEdges] You should'nt see me!");
-    return NOTHING;
-  }
-
-  return [
-    ...result,
-    ...Array.siblings
-      .map((sibling) => refreshDownEdges(nodes, [], sibling))
-      .flat(),
-  ];
-};
-
 const generateEdge = (fatherRef, childRef) => {
-  const { fromX, toX, fromY, toY } = calculateEdgePosition(fatherRef, childRef);
-
   return {
-    fromX,
-    toX,
-    fromY,
-    toY,
+    ...calculateEdgePosition(fatherRef, childRef),
     id: nanoid(),
   };
 };
 
 // TO-DO: calculate this values correctly
 const calculateEdgePosition = (fatherRef, childRef) => {
+  if (!fatherRef.current || !childRef.current) {
+    Logger.error("A node reference isn't loaded")
+
+    return {
+      fromX: 0,
+      fromY: 0,
+      toX: 0,
+      toY: 0,
+    }
+  }
+
+  // Trying to discover something with this
+  Logger.log(fatherRef.current.getBoundingClientRect())
+
   const fatherX = 0;
   const fatherY = 0;
   const childX = 0;
@@ -77,9 +38,9 @@ const calculateEdgePosition = (fatherRef, childRef) => {
 
   return {
     fromX: Math.min(fatherX, childX),
-    toX: Math.max(fatherX, childX),
     fromY: Math.min(fatherY, childY),
-    toY: Math.min(fatherY, childY),
+    toX: Math.max(fatherX, childX),
+    toY: Math.max(fatherY, childY),
   };
 };
 
@@ -92,7 +53,7 @@ const excludeNodeById = (nodes, id) => nodes.filter((n) => n.id !== id);
 
 const Actions = {
   generateNode,
-  refreshDownEdges,
+  generateEdge,
   excludeNodeById,
 };
 
